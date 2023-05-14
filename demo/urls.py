dd1b695afbade9dd018aa2015f52eb8d54304a20
Tanuj Choudhary <tanuj.rmvt@gmail.com>
2023-05-14 17:27:03 +0530

    update

--- demo/urls.py
+++ demo/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from . import views
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.views.generic.base import RedirectView
 urlpatterns=[
     path('', views.home, name=""),
     path('rec', views.rec, name="rec"), 
     path('top', views.top, name="top"),
     path('contact', views.contact, name="contact"),
-    
+    path('static/image/fav.ico', RedirectView.as_view(url=staticfiles_storage.url('image/fav.ico')))
+ 
 ]
